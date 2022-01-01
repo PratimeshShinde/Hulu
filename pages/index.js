85778f48e36558bf71d6cde9e5960af0cb76b451
Prati <pratimeshshinde121@gmail.com>
2022-01-01 19:55:46 +0530

    push

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
 import Nav from '../components/Nav';
-import Results from '../components/Results';
 import requests from '../utils/requests';
 
 export default function Home({ results }) {
@@ -17,7 +16,6 @@ export default function Home({ results }) {
 <Header />
 {/* Navigation Component */}
 <Nav />
-<Results results={results} />
     </div>
   );
 }
@@ -28,12 +26,11 @@ export async function getServerSideProps(context) {
   const request = await fetch(
     `https://api.themoviedb.org/3${
       requests[genre]?.url || requests.fetchTrending.url
-}'
-).then((res) => res.json());
-
-return {
-  props: {
-    results: request.results,
-  },
-};
-}
+    }`
+  ).then((res) => res.json());
+  return {
+    props: {
+      results: request.results,
+    },
+  };
+ }
